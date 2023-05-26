2b7fa5ce11dcc2f49d55ac046c72a3a2d850a103
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2023-05-26 16:34:32 -0400

    implemented the Cardtrick algorithm - commented out the debugging part

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -24,9 +24,9 @@ public class CardTrick {
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
-        for (Card a: hand){
-            System.out.println(a.getValue() + " " + a.getSuit());
-        }
+//        for (Card a: hand){
+//            System.out.println(a.getValue() + " " + a.getSuit());
+//        }
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here.
